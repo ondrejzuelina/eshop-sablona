b8cdae99276decfc063d1ecdce46602f4373fa2f
Ondrej Zelina <zelina.ondrej@protonmail.com>
2022-11-03 12:54:54 +0100

    pridal jsem testovaci soubor

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+echo "<pre>";
+require_once("setup/connect.php");
+
+$db = mysqli_query($connection, "SELECT * FROM product_item");
+
+while($item = mysqli_fetch_array($db)){
+    print_r($item["imagelist"]);
+    echo "<br>";
+}
+
+echo "</pre>";
