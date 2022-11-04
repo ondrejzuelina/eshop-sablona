50e225805cebaeb68bf6756f41c101b092a65bcf
Ondrej Zelina <zelina.ondrej@protonmail.com>
2022-11-04 01:44:22 +0100

    Vytvoril jsem dynamicke nav-menu

--- index.php
+++ index.php
@@ -173,50 +173,136 @@ $sql_query = ____app_sql_query("
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#!">Start Bootstrap</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <!-- <a class="navbar-brand" href="#!">Start Bootstrap</a> -->
+                <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button> -->
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="index.php?categoryid=11" role="button" data-bs-toggle="dropdown" aria-expanded="false">PC a Notebooky</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#!">All Products</a></li>
+                            <?php
+                            $result = mysqli_query($connection, "SELECT * FROM categorylist WHERE ParentCategoryCode = 11");
+                                if(!$result){
+                                    die("Print data from database error");
+                                }
+                            while($row = mysqli_fetch_array($result)){
+                            ?>
+                            <li><a class="dropdown-item" href="view_category.php?action=<?php echo $row["CategoryCode"]; ?>"><?php echo $row["CategoryName"]; ?></a></li>
+                            <?php
+                            }
+                            ?>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="index.php?categoryid=121" role="button" data-bs-toggle="dropdown" aria-expanded="false">Multifunkce a tiskárny</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="">All Products</a></li>
+                            <?php
+                            $result = mysqli_query($connection, "SELECT * FROM categorylist WHERE ParentCategoryCode = 121");
+                                if(!$result){
+                                    die("Print data from database error");
+                                }
+                            while($row = mysqli_fetch_array($result)){
+                            ?>
+                            <li><a class="dropdown-item" href="view_category.php?action=<?php echo $row["CategoryCode"]; ?>"><?php echo $row["CategoryName"]; ?></a></li>
+                            <?php
+                            }
+                            ?>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="index.php?categoryid=6" role="button" data-bs-toggle="dropdown" aria-expanded="false">Periferie</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#!">All Products</a></li>
+                            <?php
+                            $result = mysqli_query($connection, "SELECT * FROM categorylist WHERE ParentCategoryCode = 6");
+                                if(!$result){
+                                    die("Print data from database error");
+                                }
+                            while($row = mysqli_fetch_array($result)){
+                            ?>
+                            <li><a class="dropdown-item" href="view_category.php?action=<?php echo $row["CategoryCode"]; ?>"><?php echo $row["CategoryName"]; ?></a></li>
+                            <?php
+                            }
+                            ?>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="index.php?categoryid=3" role="button" data-bs-toggle="dropdown" aria-expanded="false">Komponenty</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#!">All Products</a></li>
+                            <?php
+                            $result = mysqli_query($connection, "SELECT * FROM categorylist WHERE ParentCategoryCode = 3");
+                                if(!$result){
+                                    die("Print data from database error");
+                                }
+                            while($row = mysqli_fetch_array($result)){
+                            ?>
+                            <li><a class="dropdown-item" href="view_category.php?action=<?php echo $row["CategoryCode"]; ?>"><?php echo $row["CategoryName"]; ?></a></li>
+                            <?php
+                            }
+                            ?>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="index.php?categoryid=7" role="button" data-bs-toggle="dropdown" aria-expanded="false">Servery a zálohování</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#!">All Products</a></li>
+                            <?php
+                            $result = mysqli_query($connection, "SELECT * FROM categorylist WHERE ParentCategoryCode = 7");
+                                if(!$result){
+                                    die("Print data from database error");
+                                }
+                            while($row = mysqli_fetch_array($result)){
+                            ?>
+                            <li><a class="dropdown-item" href="view_category.php?action=<?php echo $row["CategoryCode"]; ?>"><?php echo $row["CategoryName"]; ?></a></li>
+                            <?php
+                            }
+                            ?>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="index.php?categoryid=8" role="button" data-bs-toggle="dropdown" aria-expanded="false">Síťové prvky</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#!">All Products</a></li>
+                            <?php
+                            $result = mysqli_query($connection, "SELECT * FROM categorylist WHERE ParentCategoryCode = 8");
+                                if(!$result){
+                                    die("Print data from database error");
+                                }
+                            while($row = mysqli_fetch_array($result)){
+                            ?>
+                            <li><a class="dropdown-item" href="view_category.php?action=<?php echo $row["CategoryCode"]; ?>"><?php echo $row["CategoryName"]; ?></a></li>
+                            <?php
+                            }
+                            ?>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="index.php?categoryid=9" role="button" data-bs-toggle="dropdown" aria-expanded="false">Software</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#!">All Products</a></li>
+                            <?php
+                            $result = mysqli_query($connection, "SELECT * FROM categorylist WHERE ParentCategoryCode = 9");
+                                if(!$result){
+                                    die("Print data from database error");
+                                }
+                            while($row = mysqli_fetch_array($result)){
+                            ?>
+                            <li><a class="dropdown-item" href="view_category.php?action=<?php echo $row["CategoryCode"]; ?>"><?php echo $row["CategoryName"]; ?></a></li>
+                            <?php
+                            }
+                            ?>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="index.php?categoryid=9" role="button" data-bs-toggle="dropdown" aria-expanded="false">Telefony a tablety</a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <?php
+                            $result = mysqli_query($connection, "SELECT * FROM categorylist WHERE ParentCategoryCode = 18");
+                                if(!$result){
+                                    die("Print data from database error");
+                                }
+                            while($row = mysqli_fetch_array($result)){
+                            ?>
+                            <li><a class="dropdown-item" href="view_category.php?action=<?php echo $row["CategoryCode"]; ?>"><?php echo $row["CategoryName"]; ?></a></li>
+                            <?php
+                            }
+                            ?>
                             </ul>
                         </li>
                     </ul>
